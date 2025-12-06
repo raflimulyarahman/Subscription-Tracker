825690289509b296533816d22bf539fe44d39831
raflimulyarahman <mulyarahman32@gmail.com>
2025-12-06 09:09:11 +0700

    feat: refactor routes and controllers, update dependencies, and implement user authentication

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import { PORT } from './config/env.js';
 
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
-import subscriptionRouter from './routes/subscription.route.js';
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/auth', authRouter);
